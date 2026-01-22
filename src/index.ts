456069411531008fb39e8c3a7c10fd01443df56b
ShamsutOFF <shamsutoff@yandex.ru>
2026-01-22 11:28:30 +0300

    DZ 5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Happy developing ✨')
+const user = {
+    name: "Vasiliy",
+    age: 8,
+    skills: ['typescript', 'javascript']
+}
+
+const res = pickObjectKeys(user, ['age', 'skills']);
+
+/*
+Написать функцию получения нужных данных из объектов котора вернет такой объект.
+Функция должна принимать 2 аргумента: объект и массив ключей.
+Функция должна проверять что ключи существуют в объекте и возвращать новый объект с этими ключами.
+Используем дженерики и типизацию
+{
+  age: 8,
+  skills: ['typescript', 'javascript']
+}
+*/
+
+function pickObjectKeys<T extends object, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
+    return keys.reduce((acc, key) => {
+        if (key in obj) {
+            acc[key] = obj[key];
+        }
+        return acc;
+    }, {} as Pick<T, K>);
+}
+
+console.log(res);
